f7bf483aa7e52be987df5bc890f829c97151c219
Gergely Polonkai <gergely@polonkai.eu>
2016-09-06 14:07:33 +0200

    Change the duck to a list of ducks, finalize editor
    Signed-off-by: Gergely Polonkai <gergely@polonkai.eu>

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,25 @@ import { Component } from "@angular/core";
 
 import { Duck } from "./models";
 
+const DUCKS: Ducks[] = [
+    { id: 1, color: 'yellow' },
+    { id: 2, color: 'blue' },
+    { id: 3, color: 'pink' }
+]
+
 @Component({
     selector: "duckbook-front",
     templateUrl: "/app/app.component.html"
 })
 export class AppComponent {
+    ducks = DUCKS;
+    selectedDuck: Duck;
+
+    onSelect(duck: Duck): void {
+        console.log(duck);
+        this.selectedDuck = duck;
+    }
+
     duck: Duck = {
         id: 1,
         color: 'yellow'
