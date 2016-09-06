09ea2073dd4c67be285a82086aababe323a41a90
Gergely Polonkai <gergely@polonkai.eu>
2016-09-06 13:53:32 +0200

    Outline the duck editor
    Signed-off-by: Gergely Polonkai <gergely@polonkai.eu>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
 
+import { Duck } from "./models";
+
 @Component({
     selector: "duckbook-front",
     templateUrl: "/app/app.component.html"
 })
-export class AppComponent {}
+export class AppComponent {
+    duck: Duck = {
+        id: 1,
+        color: 'yellow'
+    };
+}
---
+++ app/models.ts
@@ -0,0 +1,4 @@
+export class Duck {
+    id: number;
+    color: string;
+}
